c7da22a656d379ad1e6f06f6a9abef5dacbefe8b
BhavikaBangtan <121876684+BhavikaBangtan@users.noreply.github.com>
2023-03-13 16:30:44 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,7 @@
+function addUser(){
+    player_1_name=document.getElementById("player1_name_input").value ;
+    player_2_name=document.getElementById("player2_name_input").value ;
+
+    localStorage.setItem=("player_1_name",player_1);
+    localStorage.setItem=("player_2_name",player_2);
+}
